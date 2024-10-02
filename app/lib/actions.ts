c8b7963fa7216874ed659ba2ab5da660b5a173e4
Nheil3Dev <98476350+Nheil3Dev@users.noreply.github.com>
2024-10-02 12:40:30 +0200

    Add the edit invoice page, the functionalty to delete it and handling errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,11 +25,61 @@ export async function createInvoive(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})  
   `;
+  } catch (error) {
+    return {
+      message: 'Database Error Failed to Created Invoice.'
+    }
+  }
+  
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
+}
+
+const EditInvoice = FormSchema.omit({ id: true, date: true })
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = EditInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status')
+  })
+
+  const amountInCents = amount * 100;
+
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `
+
+  } catch(error) {
+    return {
+      message: 'Database Error Failed to Update Invoice.'
+    }
+  }
+
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+}
+
+export async function deleteInvoice(id: string) {
+  try {
+    await sql`
+    DELETE FROM invoices
+    WHERE id = ${id}
+    `
+    revalidatePath('/dashboard/invoices')
+  } catch(error) {
+    return {
+      message: 'Database Error Failed to Delete Invoice.'
+    }
+  }
+
 }
