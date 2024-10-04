d8b57cdae8af2f4a7f9fe73bdc9caa58e3dbe304
Nheil3Dev <98476350+Nheil3Dev@users.noreply.github.com>
2024-10-04 09:42:50 +0200

    Add Customers Table Skeleton

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,9 @@
-import { fetchFilteredCustomers } from "@/app/lib/data";
 import CustomersTable from "@/app/ui/customers/table";
+import { lusitana } from "@/app/ui/fonts";
+import Search from "@/app/ui/search";
+import { CustomersTableSkeleton } from "@/app/ui/skeletons";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Customers",
@@ -14,10 +17,20 @@ export default async function Page({
   };
 }) {
   const query = searchParams?.query || "";
-  const customers = await fetchFilteredCustomers(query);
+
   return (
-    <main>
-      <CustomersTable customers={customers} />
-    </main>
+    <div className="w-full">
+      <h1 className={`${lusitana.className} mb-4 md:mt-8 text-2xl`}>
+        Customers
+      </h1>
+      <Search placeholder="Search customers..." />
+      <div className="mt-6 flow-root">
+        <div className="overflow-x-auto">
+          <Suspense key={query} fallback={<CustomersTableSkeleton />}>
+            <CustomersTable query={query} />
+          </Suspense>
+        </div>
+      </div>
+    </div>
   );
 }
